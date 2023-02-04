bb409c649f93d9f09e890feb725ea64419a6ef36
ricocatford <s7funoob@gmail.com>
2023-02-04 20:43:53 +0000

    Added functionality for updating sales worksheet on our spreadsheet.

--- run.py
+++ run.py
@@ -16,14 +16,19 @@ def get_sales_data():
     """
     Get sales figures input from the user
     """
-    print("Please enter sales data from the last market.")
-    print("Data should be six numbers, separated by commas.")
-    print("Example: 10,20,30,10,20,40\n")
+    while True:
+        print("Please enter sales data from the last market.")
+        print("Data should be six numbers, separated by commas.")
+        print("Example: 10,20,30,10,20,40\n")
 
-    data_str = input("Enter your data here: ")
-    sales_data = data_str.split(",")
+        data_str = input("Enter your data here: ")
+        sales_data = data_str.split(",")
 
-    validate_data(sales_data)
+        if validate_data(sales_data):
+            print("Data is valid :)")
+            break
+
+    return sales_data
 
 def validate_data(values):
     """
@@ -39,5 +44,19 @@ def validate_data(values):
 
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
+        return False
 
-get_sales_data()
+    return True
+
+def update_sales_worksheet(data):
+    """
+    Update sales worksheet, add new row with the list data provided.
+    """
+    print("Updating sales worksheet...\n")
+    sales_worksheet = SHEET.worksheet("sales")
+    sales_worksheet.append_row(data)
+    print("Sales worksheet updated successfully! Thank you.\n")
+
+data = get_sales_data()
+sales_data = [int(num) for num in data]
+update_sales_worksheet(sales_data)
