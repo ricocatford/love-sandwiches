a00a3916876422ddeeb5c4786d0dd8a86a26d3d2
ricocatford <s7funoob@gmail.com>
2023-02-04 20:27:29 +0000

    Added validation for input data, it checks if they are 6 values and convert them into integers.

--- run.py
+++ run.py
@@ -21,6 +21,23 @@ def get_sales_data():
     print("Example: 10,20,30,10,20,40\n")
 
     data_str = input("Enter your data here: ")
-    print(f"The data provided is {data_str}")
+    sales_data = data_str.split(",")
+
+    validate_data(sales_data)
+
+def validate_data(values):
+    """
+    Inside the try, converts all string values into integers.
+    Raises ValueError if values can not be converted or if they aren't exact 6 values.
+    """
+    try:
+        [int(value) for value in values]
+        if len(values) != 6:
+            raise ValueError(
+                f"Exactly 6 values are required, you provided {len(values)}"
+            )
+
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
 
 get_sales_data()
