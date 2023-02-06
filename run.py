d764beba9cfec1038de6eefdd981736408f721c9
ricocatford <s7funoob@gmail.com>
2023-02-06 18:20:32 +0000

    Added function for telling user how many of each sandwich need to be done for tomorrow.

--- run.py
+++ run.py
@@ -105,6 +105,14 @@ def calculate_stock_data(data):
 
     return new_stock_data
 
+def get_stock_values(data):
+    headings = SHEET.worksheet("stock").row_values(1)
+
+    stock_values = dict(zip(headings, data))
+    print("The number of each sandwich you need to prepare for tomorrow are:")
+    print(stock_values)
+
+
 def main():
     data = get_sales_data()
     sales_data = [int(num) for num in data]
@@ -114,6 +122,8 @@ def main():
     sales_columns = get_last_five_entries_sales()
     stock_data = calculate_stock_data(sales_columns)
     update_worksheet(stock_data, "stock")
+    get_stock_values(stock_data)
+
 
 print("Welcome to Love Sandwiches Data Automation\n")
 main()
